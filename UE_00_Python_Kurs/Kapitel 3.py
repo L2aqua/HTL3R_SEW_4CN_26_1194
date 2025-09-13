e3457fe8ab889c201d159337a13918d315c4987c
L2aqua <1194@htl.rennweg.at>
2025-09-13 14:47:22 +0200

    Kapitel 3 abgeschlossen

--- UE_00_Python_Kurs/Kapitel 3.py
+++ UE_00_Python_Kurs/Kapitel 3.py
@@ -1,2 +1,18 @@
 #Frage 1
+# Kantenlänge des Würfels in Millimeter
+cube_edge_length_mm = 5.0
+
+# Umrechnung in Zentimeter
+cube_edge_length_cm = cube_edge_length_mm / 10
+
+# Volumen des Würfels (in Kubikzentimeter)
+cube_volume_cm3 = cube_edge_length_cm ** 3
+
+# Masse des Würfels in Gramm
+cube_mass_g = 2.68
+
+# Dichte in g/cm³
+cube_density_g_per_cm3 = cube_mass_g / cube_volume_cm3
+
+print("Dichte des Platinwürfels:", cube_density_g_per_cm3, "g/cm³")
 #Frage 2
