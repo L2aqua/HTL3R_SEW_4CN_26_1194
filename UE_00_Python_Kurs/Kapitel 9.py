dd91634edef93025a5cdd919516e0053850d2a3c
L2aqua <1194@htl.rennweg.at>
2025-09-13 17:03:35 +0200

    Kapitel 9 abgeschlossen

---
+++ UE_00_Python_Kurs/Kapitel 9.py
@@ -0,0 +1,14 @@
+#Frage 1
+a: {'Bayern': 'München', 'Hessen': 'Wiesbaden'}
+
+#Frage 2
+d = {'Bayern': 'München', 'Hessen': 'Wiesbaden'}
+
+'Hessen' in d
+
+#Frage 3
+#recherchiert
+
+#Frage 4
+d = {'Lisa': 14, 'Laura': 25, 'Leon': 15, 'Leonard': 17, 'Louisa': 15, 'Lena': 13}
+print(d['Lena'])
