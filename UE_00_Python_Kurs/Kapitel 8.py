5fbfff2c343e67fb8eb3a1e4515763f125e5cdc9
L2aqua <1194@htl.rennweg.at>
2025-09-13 16:56:09 +0200

    Kapitel 8 abgeschlossen

---
+++ UE_00_Python_Kurs/Kapitel 8.py
@@ -0,0 +1,25 @@
+#Frage 1
+zahlen = [1, 6, 36, 7, 22, 80]
+
+ergebnis = zahlen[0] + zahlen[-1]
+
+print(ergebnis)
+
+#Frage 2
+zeichenketten = ['Apfel', 'Käse', 'Pizza', 'lecker', 'Kuchen', 'Eis']
+
+zusammen = zeichenketten[0] + zeichenketten[-2]
+print(zusammen)
+
+#Frage 3
+rezept = ['Eyer', 'Quag', 'Vanille-Zucker', 'Butter', 'Milch', 'Mehhl', 'Leitungswasser', 'Öl', 'Scokolad', 'Streusel', 'Kakao']
+
+rezept[0] = 'Eier'
+rezept[1] = 'Quark'
+rezept[5] = 'Mehl'
+rezept[8] = 'Schokolade'
+
+print(rezept)
+
+#Frage 4
+#Im internet recherchiert
