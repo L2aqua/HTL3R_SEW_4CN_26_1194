48af0ae13e53fafa59016ddf767e39446244d797
L2aqua <1194@htl.rennweg.at>
2025-09-13 15:36:17 +0200

    Kapitel 5 abgeschlossen

---
+++ UE_00_Python_Kurs/Kapitel 5.py
@@ -0,0 +1,25 @@
+#Frage 1
+x = 14
+if 13 < x <= 18:
+    print("Spieler nicht zu alt und nicht zu jung")
+elif x < 13:
+    print("Spieler ist zu jung")
+elif x >= 18:
+    print("Spieler ist zu alt")
+else:
+    print("Wie alt ist der Knecht amk")
+
+#Frage 2
+geschwindigkeit = 64
+if geschwindigkeit > 70:
+     print('Fahrzeug ist über dem Geschwindigkeitslimit.')
+else:
+     print('Fahrzeug ist unter dem Geschwindigkeitslimit.')
+
+#Frage 3
+a = 18 <= 18
+b = 18 < 18
+c = -5 > -20
+d = -20 > -12
+
+print(a, b, c, d)
