84c586fcfd95b64b80fe52564e1ab34f7891510c
L2aqua <1194@htl.rennweg.at>
2025-09-13 16:38:12 +0200

    Kapitel 7 abgeschlossen

---
+++ UE_00_Python_Kurs/Kapitel 7.py
@@ -0,0 +1,14 @@
+#Frage 1
+#def ausgabe(text, zähler):
+#    for i in range(zähler)
+#        print(text)
+#ausgabe('Du hast das 7. Kapitel erfolgreich abgeschlossen - Super!', 10)
+
+#Frage 2
+def ausgabe(text, zähler):
+    for i in range(zähler):
+        print(text)
+ausgabe('Du hast das 7. Kapitel erfolgreich abgeschlossen - Super!', 10)
+
+#Frage 3
+#Antwort A
