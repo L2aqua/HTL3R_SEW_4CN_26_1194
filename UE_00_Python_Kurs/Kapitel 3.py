864343c3248f0c592c031f142ebd9c5edb2b069a
L2aqua <1194@htl.rennweg.at>
2025-09-13 15:08:51 +0200

    Kapitel 3 abgeschlossen

--- UE_00_Python_Kurs/Kapitel 3.py
+++ UE_00_Python_Kurs/Kapitel 3.py
@@ -16,3 +16,19 @@ cube_density_g_per_cm3 = cube_mass_g / cube_volume_cm3
 
 print("Dichte des Platinwürfels:", cube_density_g_per_cm3, "g/cm³")
 #Frage 2
+# Ergebnis aus der Dichteberechnung
+ergebnis = 21.44
+
+# Umwandlung in eine Zeichenkette
+lösungswort = str(ergebnis)
+
+print("Das Lösungswort lautet:", lösungswort)
+#Frage 3
+# Gegebene Werte
+ladung_Q_coulomb = 64.5   # in Coulomb
+zeitdauer_t_sekunden = 5.5   # in Sekunden
+
+# Stromstärke nach I = Q / t
+stromstaerke_ampere = ladung_Q_coulomb / zeitdauer_t_sekunden
+
+print("Die Stromstärke beträgt:", stromstaerke_ampere, "A")
